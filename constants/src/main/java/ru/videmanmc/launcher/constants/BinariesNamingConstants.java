c7d8a1d1e37e539debad6c6b70479fb55c9b4fce
Алексей Видеман <41009944+AlekseyVideman@users.noreply.github.com>
2025-07-14 11:49:33 +0700

    [545] Add naming convention for the launcher binaries (#15)
    + Fix http-client module`s packaging
    + Move version settings to the new version-management plugin

---
+++ constants/src/main/java/ru/videmanmc/launcher/constants/BinariesNamingConstants.java
@@ -0,0 +1,9 @@
+package ru.videmanmc.launcher.constants;
+
+public class BinariesNamingConstants {
+
+    public static final String SECONDARY_BOOTLOADER_PREFIX = "bootloader-secondary";
+
+    public static final String LAUNCHER_PREFIX = "launcher";
+
+}
