2253df738b2020c785ac97234dfb7fd85f3b22a6
Aleksey Videman <alekseyvideman@gmail.com>
2025-08-31 15:34:23 +0700

    648 Add error modal

--- bootloader/src/main/java/ru/videmanmc/launcher/bootloader/Bootloader.java
+++ bootloader/src/main/java/ru/videmanmc/launcher/bootloader/Bootloader.java
@@ -2,11 +2,19 @@ package ru.videmanmc.launcher.bootloader;
 
 import com.google.inject.Guice;
 import com.google.inject.Inject;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
 import ru.videmanmc.launcher.bootloader.configuration.BasicGuiceConfiguration;
 import ru.videmanmc.launcher.bootloader.service.StartingService;
 import ru.videmanmc.launcher.http.client.configuration.HttpGuiceConfiguration;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Bootloader {
 
@@ -37,7 +45,78 @@ public class Bootloader {
     }
 
     void start() {
-        startingService.start();
+        try {
+            startingService.start();
+        } catch (Exception e) {
+            showErrorModal(e);
+        }
+    }
+
+    public void showErrorModal(Exception exception) {
+        var suggestion = new JLabel("Скопируйте текст ошибки ниже (Ctr + C)  и сообщите  нам");
+
+        var errorMessage = new JTextArea();
+        errorMessage.append(createUserErrorMessage(exception));
+        errorMessage.setEditable(false);
+
+        var openIssuesButton = getButton(URI.create("https://github.com/VidemanMC/launcher/issues"));
+        openIssuesButton.setMargin(new Insets(10, 30, 10, 30));
+
+        var panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(suggestion);
+        panel.add(new JScrollPane(errorMessage));
+        panel.add(openIssuesButton);
+
+        var modal = new JDialog(frame, "ОШИБКА", true);
+        modal.setLocationRelativeTo(frame);
+        modal.add(panel);
+        modal.pack();
+        modal.setVisible(true);
+    }
+
+    private @NotNull JButton getButton(URI issuesUri) {
+        var openIssuesButton = new JButton("Сообщить об ошибке");
+        openIssuesButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                openWebpage(issuesUri);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        return openIssuesButton;
+    }
+
+    private String createUserErrorMessage(Exception exception) {
+        return Arrays.stream(exception.getStackTrace())
+                     .map(StackTraceElement::toString)
+                     .collect(Collectors.joining("\n"));
+    }
+
+    @SneakyThrows
+    private void openWebpage(URI uri) {
+        var desktop = Desktop.getDesktop();
+        desktop.browse(uri);
     }
 
     /**
@@ -49,6 +128,13 @@ public class Bootloader {
     }
 
     private JFrame buildGui() {
+        Font font = new Font("Arial", Font.PLAIN, 18);
+        UIManager.put("Label.font", font);
+        UIManager.put("TextArea.font", font);
+        UIManager.put("TextField.font", font);
+        UIManager.put("Button.font", font);
+        UIManager.put("Dialog.font", font);
+
         var progress = new JProgressBar();
         progress.setIndeterminate(true);
 
