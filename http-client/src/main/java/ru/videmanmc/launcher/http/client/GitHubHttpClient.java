52f7cfecaafaebfdd6fce02ec729df7ba169e614
Алексей Видеман <41009944+AlekseyVideman@users.noreply.github.com>
2025-09-02 01:15:53 +0700

    652 Change URL to launcher releases (#22)

--- http-client/src/main/java/ru/videmanmc/launcher/http/client/GitHubHttpClient.java
+++ http-client/src/main/java/ru/videmanmc/launcher/http/client/GitHubHttpClient.java
@@ -21,15 +21,18 @@ public class GitHubHttpClient implements GameFilesClient, BinaryClient {
 
     public static final String RAW_CONTENT_MIME = "application/vnd.github.raw+json";
 
-    private static final String BASE_URL = "https://api.github.com/repos/VidemanMC/modpack";
+    private static final String MODPACK_BASE_URL = "https://api.github.com/repos/VidemanMC/modpack";
 
-    private static final String DOWNLOAD_URI_TEMPLATE = BASE_URL + "/contents/{path}";
+    private static final String DOWNLOAD_URI_TEMPLATE = MODPACK_BASE_URL + "/contents/{path}";
 
-    private static final String LATEST_RELEASES_URI = BASE_URL + "/releases/latest";
+    private static final String LAUNCHER_BASE_URL = "https://api.github.com/repos/VidemanMC/launcher";
+
+    private static final String LATEST_RELEASES_URI = LAUNCHER_BASE_URL + "/releases/latest";
+
 
     private final HttpClient httpClient = HttpClient.newBuilder()
-            .followRedirects(HttpClient.Redirect.ALWAYS)
-            .build();
+                                                    .followRedirects(HttpClient.Redirect.ALWAYS)
+                                                    .build();
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -54,11 +57,12 @@ public class GitHubHttpClient implements GameFilesClient, BinaryClient {
                 )
         );
         var request = httpBuilder.uri(uri)
-                .build();
+                                 .build();
         var bytes = httpClient.send(
-                request,
-                HttpResponse.BodyHandlers.ofByteArray()
-        ).body();
+                                      request,
+                                      HttpResponse.BodyHandlers.ofByteArray()
+                              )
+                              .body();
         var abstractFilePath = abstractPathFormatting.apply(filePath);
 
         return new GameFile(bytes, abstractFilePath);
@@ -73,11 +77,12 @@ public class GitHubHttpClient implements GameFilesClient, BinaryClient {
     public BinaryInfo getInfoByPrefix(String namePrefix) {
         var uri = URI.create(LATEST_RELEASES_URI);
         var request = httpBuilder.uri(uri)
-                .build();
+                                 .build();
         var rawReleaseInfo = httpClient.send(
-                request,
-                HttpResponse.BodyHandlers.ofString()
-        ).body();
+                                               request,
+                                               HttpResponse.BodyHandlers.ofString()
+                                       )
+                                       .body();
         var jsonNode = objectMapper.readTree(rawReleaseInfo);
 
         return constructBinary(jsonNode, namePrefix);
@@ -85,27 +90,32 @@ public class GitHubHttpClient implements GameFilesClient, BinaryClient {
 
     private BinaryInfo constructBinary(JsonNode releaseNode, String namePrefix) {
         return releaseNode.get("assets")
-                .valueStream()
-                .filter(jsonAsset -> {
-                    var assetName = jsonAsset.get("name").asText();
-                    return assetName.startsWith(namePrefix);
-                })
-                .map(jsonAsset -> {
-                    var name = jsonAsset.get("name").asText();
-                    var downloadUrl = jsonAsset.get("browser_download_url").asText();
-                    var hash = jsonAsset.get("digest").asText();
+                          .valueStream()
+                          .filter(jsonAsset -> {
+                              var assetName = jsonAsset.get("name")
+                                                       .asText();
+                              return assetName.startsWith(namePrefix);
+                          })
+                          .map(jsonAsset -> {
+                              var name = jsonAsset.get("name")
+                                                  .asText();
+                              var downloadUrl = jsonAsset.get("browser_download_url")
+                                                         .asText();
+                              var hash = jsonAsset.get("digest")
+                                                  .asText();
 
-                    return new BinaryInfo(name, downloadUrl, hash);
-                })
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("Asset '" + namePrefix + "*' is not found on GitHub"));
+                              return new BinaryInfo(name, downloadUrl, hash);
+                          })
+                          .findFirst()
+                          .orElseThrow(() -> new IllegalStateException("Asset '" + namePrefix + "*' is not found on GitHub"));
     }
 
     @SneakyThrows
     public Binary getBinary(BinaryInfo info) {
         var request = HttpRequest.newBuilder(URI.create(
-                info.downloadUrl()
-        )).build();
+                                         info.downloadUrl()
+                                 ))
+                                 .build();
         HttpResponse<byte[]> response = httpClient.send(
                 request,
                 HttpResponse.BodyHandlers.ofByteArray()
