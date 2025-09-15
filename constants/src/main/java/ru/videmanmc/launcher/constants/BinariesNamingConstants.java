269778bb891080ed8ff537357cc5552f2f06b041
Aleksey Videman <alekseyvideman@gmail.com>
2025-09-15 17:44:23 +0700

    675 Rename final jar following naming conventions

--- constants/src/main/java/ru/videmanmc/launcher/constants/BinariesNamingConstants.java
+++ constants/src/main/java/ru/videmanmc/launcher/constants/BinariesNamingConstants.java
@@ -2,8 +2,6 @@ package ru.videmanmc.launcher.constants;
 
 public class BinariesNamingConstants {
 
-    public static final String SECONDARY_BOOTLOADER_PREFIX = "bootloader-secondary";
-
     public static final String LAUNCHER_PREFIX = "launcher";
 
 }
