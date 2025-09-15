dbc20d65156d7e046cb3f14da5c4f9e38f788e2e
Aleksey Videman <alekseyvideman@gmail.com>
2025-09-15 18:37:11 +0700

    333 Calculate comfortable RAM at first startup

--- core/src/main/java/ru/videmanmc/launcher/core/model/value/Settings.java
+++ core/src/main/java/ru/videmanmc/launcher/core/model/value/Settings.java
@@ -1,9 +1,12 @@
 package ru.videmanmc.launcher.core.model.value;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
 
 import static ru.videmanmc.launcher.constants.WorkingDirectoryConstants.CLIENT_SUBDIRECTORY_PATH;
 import static ru.videmanmc.launcher.constants.WorkingDirectoryConstants.MAIN_DIRECTORY_PATH;
@@ -12,10 +15,11 @@ import static ru.videmanmc.launcher.constants.WorkingDirectoryConstants.MAIN_DIR
 @Setter
 public class Settings {
 
+    private static final int MINIMAL_COMFORTABLE_RAM = 2048;
+
     private GameSettings game = new GameSettings();
 
     private LauncherSettings startup = new LauncherSettings();
-
 
     @Getter
     @Setter
@@ -34,7 +38,18 @@ public class Settings {
         /**
          * Available RAM for running Minecraft.
          */
-        private int ramMegabytes = 2;
+        private int ramMegabytes = Math.max(MINIMAL_COMFORTABLE_RAM, calculateAvailableRamInMb());
+
+        @JsonIgnore
+        public int calculateAvailableRamInMb() {
+            OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+            if (osBean instanceof com.sun.management.OperatingSystemMXBean os) {
+                long freeMemoryBytes = os.getFreeMemorySize();
+                return Math.toIntExact(Math.round((double) freeMemoryBytes / (1024 * 1024)));
+            } else {
+                throw new UnsupportedOperationException("Not able to get free RAM");
+            }
+        }
 
     }
 
