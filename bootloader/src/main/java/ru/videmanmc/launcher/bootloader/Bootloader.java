c394bf6d68a13f43fe151ec0e111a93f0be5f9f5
Алексей Видеман <41009944+AlekseyVideman@users.noreply.github.com>
2025-09-30 18:57:11 +0700

    698 Add error dialog (#53)

--- bootloader/src/main/java/ru/videmanmc/launcher/bootloader/Bootloader.java
+++ bootloader/src/main/java/ru/videmanmc/launcher/bootloader/Bootloader.java
@@ -3,18 +3,12 @@ package ru.videmanmc.launcher.bootloader;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import lombok.SneakyThrows;
-import org.jetbrains.annotations.NotNull;
 import ru.videmanmc.launcher.bootloader.configuration.BasicGuiceConfiguration;
 import ru.videmanmc.launcher.bootloader.service.StartingService;
 import ru.videmanmc.launcher.http_client.configuration.HttpGuiceConfiguration;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.net.URI;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class Bootloader {
 
@@ -52,85 +46,8 @@ public class Bootloader {
         }
     }
 
-    public void showErrorModal(Exception exception) {
-        var suggestion = new JLabel("Скопируйте текст ошибки ниже (Ctr + C)  и сообщите  нам");
-
-        var errorMessage = new JTextArea();
-        errorMessage.append(createUserErrorMessage(exception));
-        errorMessage.setEditable(false);
-
-        var openIssuesButton = getButton(URI.create("https://github.com/VidemanMC/launcher/issues"));
-        openIssuesButton.setMargin(new Insets(10, 30, 10, 30));
-
-        var panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(suggestion);
-        panel.add(new JScrollPane(errorMessage));
-        panel.add(openIssuesButton);
-
-        var modal = new JDialog(frame, "ОШИБКА", true);
-        modal.setLocationRelativeTo(frame);
-        modal.add(panel);
-        modal.pack();
-        modal.setVisible(true);
-    }
-
-    private @NotNull JButton getButton(URI issuesUri) {
-        var openIssuesButton = new JButton("Сообщить об ошибке");
-        openIssuesButton.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                openWebpage(issuesUri);
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                // not used
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                // not used
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                // not used
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                // not used
-            }
-        });
-        return openIssuesButton;
-    }
-
-    private String createUserErrorMessage(Exception exception) {
-        var stacktrace = Arrays.stream(exception.getStackTrace())
-                               .map(StackTraceElement::toString)
-                               .collect(Collectors.joining("\n"));
-        return """
-                Message: %s
-                Stacktrace: %s
-                """
-                .formatted(
-                        exception.getMessage(),
-                        stacktrace
-                );
-    }
-
-    @SneakyThrows
-    private void openWebpage(URI uri) {
-        var desktop = Desktop.getDesktop();
-
-        if (desktop.isSupported(Desktop.Action.BROWSE)) {
-            desktop.browse(uri);
-        } else {
-            Runtime.getRuntime()
-                   .exec(new String[]{"xdg-open", uri.toString()});
-        }
+    public void showErrorModal(Exception e) {
+        //
     }
 
     /**
